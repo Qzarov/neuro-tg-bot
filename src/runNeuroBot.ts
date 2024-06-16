85a2589204cc22a031c36e8218c4a10e42b8257c
Qzarov <qzarov@mail.ru>
2024-06-16 20:54:47 +0000

    Fix Mongo connection

--- src/runNeuroBot.ts
+++ src/runNeuroBot.ts
@@ -5,8 +5,9 @@ import EventsHandler from "./handlers/events";
 import config from "./config"
 import { connectToMongo } from "./services/mongo.service";
 
-
-connectToMongo()
+(async () => {
+    await connectToMongo();
+})();
 
 const tgBot = new TgBot(config.BOT_TOKEN)
 const eventsHandler = new EventsHandler(tgBot)
@@ -19,3 +20,5 @@ tgBot.bot.on("message", async (message: Message) => {
 tgBot.bot.on("callback_query", async (callbackQuery) => {
     await eventsHandler.callbackReceived(callbackQuery)
 });
+
+console.log(`Crypto Tarot bot starts polling!`)
